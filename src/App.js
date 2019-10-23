5b9eb7b1feb4712bd9239d938f2ced781460d9a1
Priyansh <pradadiya667@gmail.com>
2019-10-23 11:51:41 -0400

    Navbar Build User Build Userlist Build Api Call Build Spinner Build For Loading

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import Navbar from './component/layout/Navbar';
+import Users from './component/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get('https://api.github.com/users');
+
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Hello to PR</h1>
+        <Navbar />
+        <div className="container">
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
     );
   }

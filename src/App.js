21aaca58b09ab0b5010d12a4da906a3d50c78e9f
Priyansh <pradadiya667@gmail.com>
2019-10-23 15:20:58 -0400

    Added clear button to clear the searched users after search Also, added the logic to display the clear button only if something is searched Built Alert.js which will display the alert Added logic for alert which will worl only if search button is clicked but their is no search text filled

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import Navbar from './component/layout/Navbar';
 import Users from './component/users/Users';
 import Search from './component/users/Search';
+import Alert from './component/layout/Alert';
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   };
 
   //Search Users
@@ -20,13 +22,28 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  //clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false });
+
+  // Set Alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+  };
+
   render() {
+    const { users, loading } = this.state;
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Search searchUsers={this.searchUsers} />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Alert alert={this.state.alert} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+          />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );

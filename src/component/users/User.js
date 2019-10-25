2e402b6bb8f0b9605df3c3d04bda7985dabb856b
Priyansh <pradadiya667@gmail.com>
2019-10-24 20:11:01 -0400

    Added a Link to user.js to go back to seacrh page Also added logo for whether he is hireable or not

--- src/component/users/User.js
+++ src/component/users/User.js
@@ -1,9 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
   }
+
+  static propTypes = {
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired
+  };
+
   render() {
     const {
       name,
@@ -21,7 +31,23 @@ class User extends Component {
     } = this.props.user;
 
     const { loading } = this.props;
-    return <div>{name}</div>;
+
+    if (loading) return <Spinner />;
+    else {
+      return (
+        <Fragment>
+          <Link to="/" className="btn btn-light">
+            Back To Search
+          </Link>
+          Hireable:{' '}
+          {hireable ? (
+            <i className="fas fa-check text-success" />
+          ) : (
+            <i className="fas fa-times-circle text-danger" />
+          )}
+        </Fragment>
+      );
+    }
   }
 }
 

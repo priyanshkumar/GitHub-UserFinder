688d8c7e4a7f4f8de0b17026eae521565075fd04
Priyansh <pradadiya667@gmail.com>
2019-10-26 02:27:48 -0400

    added a githubContext.Provider and passed all state variables to value

--- src/context/github/githubState.js
+++ src/context/github/githubState.js
@@ -15,10 +15,33 @@ const GithubState = props => {
     users: [],
     user: {},
     repos: [],
-    loading: false,
-    alert: null
+    loading: false
   };
 
   const [state, dispatch] = useReducer(GithubReducer, initialState);
 
+  //Search User
+
+  //Get User
+
+  // Get Repos
+
+  // Clear Users
+
+  // Set Loading
+
+  return (
+    <GithubContext.Provider
+      value={{
+        users: state.users,
+        user: state.user,
+        repos: state.repos,
+        loading: state.loading
+      }}
+    >
+      {props.children}
+    </GithubContext.Provider>
+  );
 };
+
+export default GithubState;
